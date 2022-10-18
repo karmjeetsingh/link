5ffb6d59ceac5eaa0626c7d619dbd92bc4897b24
Karmjeet Singh <karmjeetsingh216@gmail.com>
2022-10-18 09:28:25 -0400

    swift code added

---
+++ fileOnTry.swift
@@ -0,0 +1,13 @@
+print("")
+Class Bg : @UIApplicationMain {}
+struct Gi{
+    
+}
+for i in 1...6{
+    print(i*i)
+}
+let option binding
+for wrapping
+        let o = 5
+        var k = o!
+        
